7772e87effc37a82c5bac4509b435696864593a3
Kevin Patchane-Lacane <kevinpl07@gmail.com>
2020-08-21 21:47:37 +0200

    stuff still a lil'bit broken but we're getting there

--- JS/cart.js
+++ JS/cart.js
@@ -76,3 +76,57 @@ function updateTotal() {
     totalPrice.innerText = "Total : " + total/100 + ',00€';
 }
 
+let formData = [];
+let cartData = {};
+let dataToSend = {};
+let sendBtn = document.getElementById('valider');
+
+sendBtn.addEventListener('click', () => {
+    formData.push(nom.value);
+    formData.push(prenom.value);
+    formData.push(address.value);
+    formData.push(city.value);
+    formData.push(mail.value);
+
+    localStorage.setItem("user", JSON.stringify(formData));
+})
+
+
+const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+  
+      xhr.responseType = 'json';
+  
+      if (data) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
+  
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      };
+  
+      xhr.onerror = () => {
+        reject('Something went wrong!');
+      };
+  
+      xhr.send(JSON.stringify(data));
+    });
+    return promise;
+  };
+  
+
+const sendData = () => {
+    sendHttpRequest('POST', 'http://localhost:3000/api/teddies/order', dataToSend)
+      .then(responseData => {
+        console.log(responseData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
