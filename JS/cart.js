7087bdf3ef2b5d3e194738ad2e9145207c6ee258
Kevin Patchane-Lacane <kevinpl07@gmail.com>
2020-08-23 16:46:53 +0200

    cart, promise, post

--- JS/cart.js
+++ JS/cart.js
@@ -1,18 +1,9 @@
 // Clear cart sur cart.html
-if (document.readyState == 'loading') {
+if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', clearCart)
 } else {
   clearCart();
-
 }
-
-
-// On récupère les oursons
-getTeddy().then(teddies => {
-  displayInCart(teddies);
-});
-
-
 
 //fonction qui vide le localStorage et nous ramène à la page d'accueil
 function clearCart() {
@@ -25,45 +16,35 @@ function clearCart() {
     window.location.href = "index.html";
   })
 }
-// définition de l'objet avec la clé "ours" du localStorage
+
+
 let ours = JSON.parse(localStorage.getItem('ours'));
-
 let tbody = document.getElementById('corps');
-var total = 0;
+let total = 0;
 
-console.log(typeof (ours));
-console.log(ours[0].price);
+ours.forEach(teddies => {
+  let row = document.createElement('tr');
+  row.className = "cartRows";
+  let cell1 = document.createElement('td');
+  let cell2 = document.createElement('td');
+  let cell3 = document.createElement('td');
+  cell3.className = "itemPrice";
+  let cell4 = document.createElement('td');
+  cell4.className = "itemQuantity";
+  let img = document.createElement('img');
 
-// fonction qui crée des lignes du tableau 
-// par rapport au nombre d'oursons dans le panier
-function displayInCart(teddies) {
-  for (i = 0; i < ours.length; i++) {
-    let row = document.createElement('tr');
-    row.className = "cartRows";
-    let cell1 = document.createElement('td');
-    let cell2 = document.createElement('td');
-    let cell3 = document.createElement('td');
-    cell3.className = "itemPrice";
-    let cell4 = document.createElement('td');
-    cell4.className = "itemQuantity";
-    let img = document.createElement('img');
-
-    tbody.appendChild(row);
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    row.appendChild(cell4);
-    cell2.appendChild(img);
-    cell1.innerText = ours[i].name;
-    img.src = ours[i].imageUrl;
-    img.className = "img-mini";
-    cell3.innerText = ours[i].price / 100 + ",00€";
-    cell4.innerText = ours[i].quantity;
-
-  }
-  updateTotal();
-}
-
+  tbody.appendChild(row);
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+  row.appendChild(cell4);
+  cell2.appendChild(img);
+  cell1.innerText = teddies.name;
+  img.src = teddies.imageUrl;
+  img.className = "img-mini";
+  cell3.innerText = teddies.price / 100 + ",00€";
+  cell4.innerText = teddies.quantity;
+})
 function updateTotal() {
   var totalPrice = document.getElementsByClassName('total')[0];
   var cartRows = document.getElementsByClassName('cartRows');
@@ -75,52 +56,70 @@ function updateTotal() {
   }
   totalPrice.innerText = "Total : " + total / 100 + ',00€';
 }
+updateTotal()
 
-
-
-
-function orderId() {
-  for (let i = 0; i < ours.length; i++) {
-    products.push(ours[i].id);
-  }
-}
-let prenom = document.getElementById('prenom');
-let nom = document.getElementById('nom');
-let address = document.getElementById('address');
-let city = document.getElementById('city');
-let mail = document.getElementById('mail');
-let products = [];
-let data = {};
-let contact = {};
-
+let form = document.getElementById('form')
 let valider = document.getElementById('valider');
-valider.addEventListener('click', () => {
-    contact = {
+
+valider.addEventListener('click', (e) => {
+  if (!form.checkValidity()) {
+    e.preventDefault();
+    alert('Invalid form')
+  } 
+
+  let products = [];
+  let data = {};
+  let contact = {};
+
+  let prenom = document.getElementById('prenom');
+  let nom = document.getElementById('nom');
+  let address = document.getElementById('address');
+  let city = document.getElementById('city');
+  let mail = document.getElementById('mail');
+
+  contact = {
     firstName: prenom.value,
     lastName: nom.value,
     address: address.value,
     city: city.value,
     email: mail.value
   };
-  orderId();
-    data = {
-    contact: contact,
-    products: products
- }; 
-    console.log(data);
-    console.log(contact);
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/api/teddies/order');
-    xhr.responseType = 'json';
-    xhr.send(JSON.stringify(data));
+  for (let i = 0; i < ours.length; i++) {
+    products.push(ours[i].id);
+  }
+  data.products = products;
+  data.contact = contact;
 
-    xhr.onload = () => {
-      console.log(xhr.response);
-    }
+  function sending(url, order) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      request.onreadystatechange = function (response) {
+        if (this.readyState === 4) {
+          if (this.status === 201) {
+            resolve(response = JSON.parse(this.responseText))
+          } else {
+            reject(
+              alert('Something went wrong')
+            );
+          }
+        }
+      };
+      request.open("POST", url);
+      request.setRequestHeader("Content-Type", "application/json");
+      request.send(JSON.stringify(order));
+    });
+  };
+
+  sending(api + "/order", data)
+    .then((response) => {
+      localStorage.setItem('orderId', JSON.stringify(response.orderId))
+      window.location.href ="confirmation.html"
+    })
+    .catch()
+
+  
 })
-
-console.log(localStorage);
 
 
 
---
+++ JS/confirm.js
@@ -0,0 +1,8 @@
+let prenom = document.getElementById('confirm-firstName');
+let nom = document.getElementById('confirm-lastName');
+let adresse = document.getElementById('confirm-address');
+let email = document.getElementById('confirm-mail');
+let recap = document.getElementById('recap');
+
+console.log(localStorage); 
+
